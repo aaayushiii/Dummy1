cc2f246700d0421874ca38b93a37b57f0b666948
ayushi.sharma2 <ayushi.sharma2@GRLPLE0037>
2023-07-06 19:16:26 +0530

    commit from code

--- converted/dumb_converted.py
+++ converted/dumb_converted.py
@@ -1,12 +1,5 @@
 from pyspark.sql import SparkSession
-import time
 
 spark = SparkSession.builder.getOrCreate()
 
-start_time = time.time()
-
 print("Hello")
-
-end_time = time.time()
-execution_time = end_time - start_time
-print("Execution time:", execution_time)
